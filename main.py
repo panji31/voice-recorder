23b401f69d8fe0b3d9865bc93d32f2cc34a9688d
panji008 <gumilarp@gmail.com>
2023-06-08 10:52:56 +0700

    master

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    if request.method == "POST":
+        f = request.files['audio_data']
+        with open('audio.wav', 'wb') as audio:
+            f.save(audio)
+        print('file uploaded successfully')
+
+        return render_template('index.html', request="POST")
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=3100)
